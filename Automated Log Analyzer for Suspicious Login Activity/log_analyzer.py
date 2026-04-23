5343b816d113318ae08b699f4fea42f0011b0d75
Adrian Tan <61336517+adriantanasse@users.noreply.github.com>
2026-04-23 20:18:24 +0200

    Update log_analyzer.py

--- Automated Log Analyzer for Suspicious Login Activity/log_analyzer.py
+++ Automated Log Analyzer for Suspicious Login Activity/log_analyzer.py
@@ -1,11 +1,14 @@
+# Read the log file (IPs in our case)
 with open("login_logs.txt", "r") as file:
     logs = file.read()
 
+# Parse the logs
 lines = logs.split("\n")
 
 failed_count = 0
 alerts = []
 
+# Extract useful data
 for line in lines[1:]:
     if line:
         parts = line.split(",")
@@ -16,16 +19,24 @@ for line in lines[1:]:
 
         hour = int(time.split(":")[0])
 
+        # Detect suspicious activity
+        ## Failed login tracking
+        
         if status == "failed":
             failed_count += 1
+            # Store flagged results
             alerts.append(line)
-
+            
+        ##  Detect unusual hours
         if hour < 6 or hour > 22:
+            # Store flagged results
             alerts.append("Unusual time: " + line)
 
+# Detect brute-force attacks
 if failed_count > 3:
     print("Possible brute-force attack detected")
 
+# Write all alerts to a file – alerts.txt
 with open("alerts.txt", "w") as file:
     for alert in alerts:
         file.write(alert + "\n")
